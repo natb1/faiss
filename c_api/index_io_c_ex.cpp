f0bbc0657f633d50e2c22f280ec9ec3ce2c92b8d
Abhi Dangeti <abhinav@couchbase.com>
2023-09-13 10:17:31 -0600

    Introducing index IO operations over char buffer (#2)
    * index io operations on char* buffer
    
    * bug fix: fill the index content to an unsigned char buf
    
    * changes with respect to read API for index io
    
    * Move read/write APIs from/to buffer to separate file
    
    ---------
    
    Co-authored-by: Thejas-bhat <thejas.orkombu@couchbase.com>

---
+++ c_api/index_io_c_ex.cpp
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+//  Copyright 2023-present Facebook. All Rights Reserved
+// -*- c++ -*-
+// More I/O code for indexes
+
+#include "index_io_c_ex.h"
+#include <faiss/index_io.h>
+#include <faiss/impl/io.h>
+#include "macros_impl.h"
+
+using faiss::Index;
+using faiss::IndexBinary;
+
+int faiss_write_index_buf(const FaissIndex* idx, int* size, unsigned char** buf) {
+    try {
+        faiss::VectorIOWriter writer;
+
+        faiss::write_index(reinterpret_cast<const Index*>(idx), &writer);
+        unsigned char* tempBuf = (unsigned char*)malloc((writer.data.size() + 1) * sizeof(uint8_t));
+        std::copy(writer.data.begin(), writer.data.end(), tempBuf);
+        tempBuf[writer.data.size()] = 0;
+
+        // return the serialized index content to the passed buf
+        // furthermore, return its size (perhaps useful for memory management)
+        *buf = tempBuf;
+        *size = writer.data.size();
+    }
+    CATCH_AND_HANDLE
+}
+
+int faiss_read_index_buf(const unsigned char* buf, int size, int io_flags, FaissIndex** p_out) {
+    try {
+        faiss::VectorIOReader reader;
+        reader.data.resize(size);
+        memcpy(reader.data.data(), buf, size);
+
+        auto index = faiss::read_index(&reader, io_flags);
+        *p_out = reinterpret_cast<FaissIndex*>(index);
+    }
+    CATCH_AND_HANDLE
+}
---
+++ c_api/index_io_c_ex.h
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+//  Copyright 2023-present Facebook. All Rights Reserved
+// -*- c++ -*-
+// More I/O code for indexes
+
+#ifndef FAISS_INDEX_IO_C_EX_H
+#define FAISS_INDEX_IO_C_EX_H
+
+#include <stdio.h>
+#include "IndexBinary_c.h"
+#include "Index_c.h"
+#include "faiss_c.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** Write index to buffer
+ */
+int faiss_write_index_buf(const FaissIndex* idx, int* buf_size, unsigned char** buf);
+
+/** Read index from buffer
+ */
+int faiss_read_index_buf(const unsigned char* buf, int limit, int io_flags,
+        FaissIndex** p_out);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
